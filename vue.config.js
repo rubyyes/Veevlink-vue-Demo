18a50ac6aa5e69004a41719cfcc054d9646fe895
rubyyes <996786291@qq.com>
2019-04-10 11:56:22 +0800

    修改vue.config配置

--- vue.config.js
+++ vue.config.js
@@ -22,7 +22,7 @@ module.exports = {
 
   // 那么将这个值改为 '/my-app/'
 
-  baseUrl: "./", // 构建好的文件输出到哪里
+  publicPath: "./", // 构建好的文件输出到哪里
 
   outputDir: "dist", // where to put static assets (js/css/img/font/...) // 是否在保存时使用‘eslint-loader’进行检查 // 有效值: true | false | 'error' // 当设置为‘error’时，检查出的错误会触发编译失败
 
